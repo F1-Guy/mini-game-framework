caa6c7e4f64ff10e5631ff5f123ae61667df781d
F1-Guy <bartol.cagalj@gmail.com>
2023-04-18 13:20:53 +0200

    Implemented logger Add - Singletong logger Add - Loging methods

--- minigame-library/Objects/Entity.cs
+++ minigame-library/Objects/Entity.cs
@@ -4,12 +4,15 @@ namespace minigame_library.Objects
 {
     public abstract class Entity
     {
+        Logger _logger = Logger.GetInstance();
+
         public Entity(string name, Position startPosition, int startHealth, List<Item>? startInventory)
         {
             Name = name;
             Position = startPosition;
             Health = startHealth;
             Inventory = startInventory ?? new List<Item>();
+            _logger.Log(TraceEventType.Information, $"Entity of type was created at position {startPosition}");
         }
 
         public int Id { get; set; }
